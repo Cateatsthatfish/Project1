62914f5f65969743ab0e6b9bf814203965679a5e
Corrine <867155532@qq.com>
2020-11-03 23:51:17 +0800

    [new function add] time measure

--- try/requirement1.cpp
+++ try/requirement1.cpp
@@ -1,9 +1,6 @@
-//2020.11.3
 
-//2020.10.28
 #include<iostream>
-#include<chrono> //计算时间方法一【failed】
-#include<Windows.h> //计算时间方法二
+#include<chrono> //计算时间方法一
 
 
 using namespace std;
@@ -11,7 +8,7 @@ struct Matrix{
     long long row;
     long long column;
     long long total ;
-    float * datas = new float[total];
+    float * datas ;
 };
 
 Matrix & multiplication(const Matrix & A, const Matrix & B, Matrix &C);
@@ -20,35 +17,26 @@ void initial_Matrix(Matrix &A);
 
 int main(){
     ////////////initialization type 2 ->use set function
-    float time = 1.0;
-
     struct Matrix A;
     initial_Matrix(A);
-    //display_Matrix(A);
+    display_Matrix(A);
 
     struct Matrix B;
     initial_Matrix(B);
-    //display_Matrix(B);
+    display_Matrix(B);
     
-    cout << "before C?" << endl;
     struct Matrix C ;
-    cout << "after C?"<< endl;
-    //cout << "interrupt" << endl;
-    
-    
-    auto t1=std::chrono::steady_clock::now();
-    cout << "trouble?" << endl;
+
+    auto t1=std::chrono::steady_clock::now(); //开始时间
     
     C = multiplication(A,B,C);
-    
-    auto t2=std::chrono::steady_clock::now(); 
-    cout << "trouble" << endl;
-    //double time=std::chrono::duration<double,std::milli>(t2-t1).count();
-    
+   
+    auto t2=std::chrono::steady_clock::now(); //结束时间
+    double time=std::chrono::duration<double,std::milli>(t2-t1).count();
     cout << "(time: " << time << "ms)" << endl;
+   
     
-    
-    display_Matrix(C);
+    //display_Matrix(C);
 
     //判定可以相乘的条件
     //if(A.column == B.row){    }
@@ -65,9 +53,9 @@ Matrix & multiplication(const Matrix & A, const Matrix & B, Matrix &C){
     C.row = A.row;
     C.column = B.column;
     C.total = C.row * C.column;
+    C.datas = new float[C.total];
     long long n = A.column;
-    C.datas = new float[C.total]; 
-
+    
     for(long long i = 0; i < C.row ; i++ ){
         for(long long j = 0; j < C.column; j++ ){
             float temp = 0; //// long double (最后计算出来的C可以是long double 吗？)
@@ -85,9 +73,11 @@ Matrix & multiplication(const Matrix & A, const Matrix & B, Matrix &C){
 
 }
 void display_Matrix(const Matrix & A){
-    cout << "row = " << A.row << endl;
-    cout << "column =" << A.column << endl;
-    cout << "total = " << A.total << endl;
+    //cout << "row = " << A.row << endl;
+    //cout << "column =" << A.column << endl;
+    //cout << "total = " << A.total << endl;
+    cout << "location = " << &A << endl;
+    cout << "location of data = " << &A.datas <<endl;
     for(int i = 0; i< A.total; i++){
         cout << A.datas[i] << " ";
     }
@@ -99,7 +89,7 @@ void initial_Matrix(Matrix &A){
     A.row = 2;
     A.column = 2;
     A.total = A.row * A.column;
-    A.datas = new float[A.total];
+    A.datas = new float [A.total];
     for(int i = 0; i< A.total;i++){
         A.datas[i] = i;
     }
