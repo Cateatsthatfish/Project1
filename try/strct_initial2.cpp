939f875ac08f899476113607e3c77950748d900b
Corrine <867155532@qq.com>
2020-11-04 10:13:18 +0800

    [new function]验证输入的矩阵的row&column的值是否是非零整数； [unfinished] 随即初始化矩阵

--- try/strct_initial2.cpp
+++ try/strct_initial2.cpp
@@ -1,17 +1,18 @@
 #include<chrono> //time counting
 #include<iostream>
-
+#include <string> //getline()
 using namespace std;
 struct Matrix{
     long long row;
     long long column;
     long long total ;
-    float * datas = new float[total];
+    float * datas ;
 };
 
 Matrix & multiplication(const Matrix & A, const Matrix & B);
 void display_Matrix(const Matrix & A);
 void initial_Matrix(Matrix &A);
+bool isValid_int(string in);
 
 int main(){
     ////////////initialization type 2 ->use set function
@@ -42,11 +43,55 @@ void display_Matrix(const Matrix & A){
 
 ////这里初始化的内容待改进
 void initial_Matrix(Matrix &A){
-    A.row = 2;
-    A.column = 2;
+    string n1_in, m1_in; 
+    int n1,m1;
+    cout << "initialize a m*n matrix:(m,n are positive integers)" << endl;
+    cout << "m1= ";
+    getline(cin,m1_in);
+    while(!isValid_int(m1_in)){
+    cout << "Invalid input! please try again! "<<endl;
+    cout << "m1= ";
+    getline(cin,m1_in);    
+        }
+
+    cout << "n1= ";
+    getline(cin,n1_in);
+    while(!isValid_int(n1_in)){
+    cout << "Invalid input! please try again! "<<endl;
+    cout << "n1= ";
+    getline(cin,n1_in);      
+        }
+
+    m1 = atoi(m1_in.c_str());
+    n1 = atoi(n1_in.c_str());
+
+    A.row = m1;
+    A.column = n1;
     A.total = A.row * A.column;
+    A.datas = new float [A.total];
     for(int i = 0; i< A.total;i++){
         A.datas[i] = i+1;
     }
 
+}
+
+bool isValid_int(string in)
+{
+    bool len = true;
+    if(in.length()==0){
+        len = false;
+    
+    }
+    // 不允许第一位是0的情况,与不允许输入0
+    if(in[0]=='0'){
+        len = false;
+    }
+    for(int i = 0 ; i< in.length();i++){
+        if(in[i]>'9'||in[i]<'0')
+        {
+            len = false;
+            break;
+        }
+    }    
+    return len;
 }
