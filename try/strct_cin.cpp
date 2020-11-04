257328696fae2c41cf9b49124d62f9a0601662fa
Corrine <867155532@qq.com>
2020-11-04 10:02:07 +0800

    [test]判定输入一定是Int（不允许0，空格，回车，其他符号，负数，科学计数法等）

--- try/strct_cin.cpp
+++ try/strct_cin.cpp
@@ -1,28 +1,56 @@
 #include <iostream>
-using namespace std;
-int main(){
-    int n1,m1;
-    cout << "Please initialize the first matrix with (n1,m1):" << endl;
-    cout << "n1= ";
-    cin >> n1;
-    if(!cin){
-        cout << "wrong input" << endl;
-        cin.clear();
-        cin.sync();
-    }else{
-        cout <<"n1 = " << n1 << endl;
-    }    
+#include <string>
 
+
+using namespace std;
+bool isValid_int(string in);
+
+int main(){
+    string n1_in, m1_in; 
+    int n1,m1;
+    cout << "initialize a m*n matrix:(m,n are positive integers)" << endl;
     cout << "m1= ";
-    cin >> m1;
-    if(!cin){
-        cout << "wrong input" << endl;
-        cin.clear();
-        cin.sync();
-    }else{
-        cout <<"m1 = " << n1 << endl;
-    }
+    getline(cin,m1_in);
+    while(!isValid_int(m1_in)){
+    cout << "Invalid input! please try again! "<<endl;
+    cout << "m1= ";
+    getline(cin,m1_in);    
+        }
+
+    cout << "n1= ";
+    getline(cin,n1_in);
+    while(!isValid_int(n1_in)){
+    cout << "Invalid input! please try again! "<<endl;
+    cout << "n1= ";
+    getline(cin,n1_in);      
+        }
+
+    m1 = atoi(m1_in.c_str());
+    n1 = atoi(n1_in.c_str());
+    
     return 0;
+}
+bool isValid_int(string in)
+{
+    bool len = true;
+    if(in.length()==0){
+        len = false;
+    
+    }
+    // 不允许第一位是0的情况,与不允许输入0
+    if(in[0]=='0'){
+        len = false;
+    }
+    for(int i = 0 ; i< in.length();i++){
+        if(in[i]>'9'||in[i]<'0')
+        {
+            len = false;
+            break;
+        }
+    }
+       
+    
+    return len;
 }
 /*
 test:
