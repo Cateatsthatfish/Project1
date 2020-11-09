ddfd6b8c02f6d6db7791af53cf889a983463b2b0
Corrine <867155532@qq.com>
2020-11-09 23:43:39 +0800

    [try]试着把C的其他参数初始化放到multi函数的外面，发现速度稍微慢了一点

---
+++ try/strct_setNew.cpp
@@ -0,0 +1,123 @@
+//在strct_multi.cpp的基础上分离C的参数初始化和c的float的初始化
+//2020.11.9
+#include<chrono> //计算时间
+#include<iostream>
+using namespace std;
+struct Matrix{
+    long long row;
+    long long column;
+    long long total ;
+    float * datas;
+};
+
+Matrix & multiplication(const Matrix & A, const Matrix & B, Matrix &C);
+void display_Matrix(const Matrix & A);
+void initial_MatrixA(Matrix &A);
+void initial_MatrixB(Matrix &A);
+//void initial_MatrixC(const Matrix & A, const Matrix & B, Matrix &C);
+
+int main(){
+    ////////////initialization type 2 ->use set function
+    Matrix A;
+    initial_MatrixA(A);
+
+    Matrix B;
+    initial_MatrixB(B);
+
+    Matrix C;
+
+    C.row = A.row;
+    C.column = B.column;
+    C.total = A.row*B.column;
+    C.datas = new float[C.total]();
+
+    C = multiplication(A,B,C);
+
+    for(int i = 0; i< 5; i++){
+        cout << 1+i <<":";
+        C = multiplication(A,B,C);
+    }
+
+    cout << "finished!" <<endl;
+    //display_Matrix(C);
+
+    delete [] A.datas;
+    delete [] B.datas;
+    delete [] C.datas;
+
+    //Matrix C = multiplication()
+    return 0;
+}
+
+Matrix & multiplication(const Matrix & A, const Matrix & B, Matrix &C){
+
+    long long m = A.row;
+    long long n = A.column;
+    long long l = B.column;
+    /*
+    C.row = m;
+    C.column = l;
+    C.total = m*l;
+    C.datas = new float[C.total]();
+    */
+
+    auto t1=std::chrono::steady_clock::now(); //开始时间
+    for(long long i = 0; i < m ; i++ ){
+        for(long long j = 0; j < l; j++ ){
+            float temp = 0; 
+            for (long long k = 0; k< n; k++){
+                //cout << "i="<< i << " j="<< j << " k= "<< k ;
+                // cout << " k+an*i: " << k+an*i << " j+bn*k: " << j+bn*k ;
+                //cout << "A["<< n*i+k << "] " << "B["<< l*k+j << "] ";
+
+                temp += A.datas[n*i+k] * B.datas[l*k+j];
+                //cout << A.datas[n*i+k] << "*" << B.datas[l*k+j] << " + " ;
+                }
+                //cout << " ->C["<< l*i+j << "]= "<<temp << endl;   
+
+                //cout << " j+ bn*i: " << j+ bn*i << endl;
+                C.datas[l*i+j] = temp;  
+        }
+    }
+    auto t2=std::chrono::steady_clock::now(); //结束时间
+    double time=std::chrono::duration<double,std::milli>(t2-t1).count();
+    cout << "(time: " << time << "ms)" << endl;
+
+    return C;
+
+}
+void display_Matrix(const Matrix & A){
+    cout << "row = " << A.row << endl;
+    cout << "column =" << A.column << endl;
+    cout << "total = " << A.total << endl;
+    long long n = A.column;
+    for(long long i = 0; i< A.row;i++){
+        for(long long j =0 ; j < A.column ; j++){
+        cout << A.datas[i*n+j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+////这里初始化的内容待改进
+void initial_MatrixA(Matrix &A){
+    A.row = 1400;
+    A.column = 1400;
+    A.total = A.row * A.column;
+    A.datas = new float[A.total]();
+    for(long long i = 0; i< A.total;i++){
+        A.datas[i] = i;
+    }
+
+}
+void initial_MatrixB(Matrix &A){
+    A.row = 1400;
+    A.column = 1400;
+    A.total = A.row * A.column;
+    A.datas = new float[A.total]();
+    for(long long i = 0; i< A.total;i++){
+        A.datas[i] = i;
+    }
+
+}
