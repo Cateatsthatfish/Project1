6742dacb51884368aa00c8611c6508f4b7bedbb2
Corrine <867155532@qq.com>
2020-11-10 00:57:00 +0800

    [add]按列cout

--- try/strct_display.cpp
+++ try/strct_display.cpp
@@ -10,6 +10,7 @@ struct Matrix{
 
 
 void display_Matrix(const Matrix & A);
+void display_MatrixB(const Matrix & A);
 void initial_MatrixA(Matrix &A);
 void initial_MatrixB(Matrix &A);
 
@@ -24,10 +25,18 @@ int main(){
     return 0;
 }
 
+//按照行来输出（先从左到右，再从上到下）
 void display_Matrix(const Matrix & A){
+    //基本信息：
     cout << "row = " << A.row << endl;
     cout << "column =" << A.column << endl;
     cout << "total = " << A.total << endl;
+    //数组：
+    for(int i = 0 ; i< A.total;i++){
+        cout << A.datas[i] << ",";
+    }
+    cout << endl;
+    //以矩阵形式展出：
     long long n = A.column;
     for(long long i = 0; i< A.row;i++){
         for(long long j =0 ; j < A.column ; j++){
@@ -38,13 +47,30 @@ void display_Matrix(const Matrix & A){
     cout << endl;
 }
 
+//按照列来输出（先从上到下，再从左到右）
+void display_MatrixB(const Matrix & A){
+    for(int i = 0 ; i< A.total;i++){
+        cout << A.datas[i] << ",";
+    }
+    cout << endl;
+    long long n = A.column;
+    for(long long j =0 ; j < A.column ; j++){
+        for(long long i = 0; i< A.row;i++){
+        
+        cout << A.datas[i*n+j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
 ////这里初始化的内容待改进
 void initial_MatrixA(Matrix &A){
-    A.row = 5;
+    A.row = 4;
     A.column = 3;
     A.total = A.row * A.column;
     for(long long i = 0; i< A.total;i++){
-        A.datas[i] = 1;
+        A.datas[i] = i;
     }
 
 }
