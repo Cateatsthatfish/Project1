6cc1bde748f73f4970ec4df008bccfe06d210261
Corrine <867155532@qq.com>
2020-11-13 07:42:10 +0800

    test

---
+++ openblas/test_sizeof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout << sizeof(float) <<endl; //4
+    cout << sizeof(double) <<endl; //8
+    cout << sizeof(float*) <<endl; //8 
+    cout << sizeof(double*) << endl; //8
+    return 0;
+
+}
